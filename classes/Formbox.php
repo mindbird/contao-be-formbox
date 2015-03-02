60197739acb9ed3c61498698d3435c245f5688fa
Wusch <florian.otto@mindbird.de>
2015-03-02 10:57:06 +0100

    Changed sender and button color

--- classes/Formbox.php
+++ classes/Formbox.php
@@ -14,22 +14,24 @@ class Formbox extends \BackendModule {
     
     public function showPopup() {
         $objTemplate = new \BackendTemplate('be_formbox');
+        $objUser = \BackendUser::getInstance();
         if (\Input::post('FORM_SUBMIT') == 'formbox') {
             $objDate = new \Date();
             $objEmail = new \Email();
-            $objEmail->subject = $GLOBALS['TL_CONFIG']['be_formbox_button_text'];
-            $strHtml = '<p>User: ' . \Input::post('username') . '</p>';
+            $objEmail->subject = $GLOBALS['TL_CONFIG']['websiteTitle'] . ' - ' . $GLOBALS['TL_CONFIG']['be_formbox_button_text'];
+            $strHtml = '<p>User: ' . $objUser->username . '</p>';
             $strHtml .= '<p>Site: ' . \Input::post('url') . '</p>';
             $strHtml .= '<p>Datum: ' . $objDate->datim . '</p>';
             $strHtml .= '<p>Message: ' . \Input::post('message') . '</p>';
             $objEmail->html =  $strHtml;
+            $objEmail->fromName = $objUser->name;
+            $objEmail->from =  $objUser->email;
+            //$objEmail->replyTo($objUser->name . ' <' . $objUser->email . '>');
             $objEmail->sendTo($GLOBALS['TL_CONFIG']['be_formbox_email']);
             $objTemplate->strMessageSent = $GLOBALS['TL_CONFIG']['be_formbox_message_sent'];
         }
-        $objUser = \BackendUser::getInstance();
         $objTemplate->strFormUrl = 'contao/main.php?do=settings&key=be-formbox&nb=1&popup=1'; 
         $objTemplate->strUrl = base64_decode(\Input::get('link'));
-        $objTemplate->strUser = $objUser->username;
         $objTemplate->strFormboxMessage = $GLOBALS['TL_CONFIG']['be_formbox_message'];
         return $objTemplate->parse();
     }
